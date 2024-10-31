873a1c1663ea6fb429274f79b0c8267e79eeddd5
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-10-31 11:11:59 -0400

    feat: created a Google Map to show the city location

---
+++ app/(dashboaard)/city/Map.tsx
@@ -0,0 +1,30 @@
+'use client';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import CircularLoader from '../../components/CircularLoader/CircularLoader';
+
+type TMap = {
+    lat: number,
+    lon: number
+}
+
+export default function Map({
+    lat,
+    lon: lng
+}: Readonly<TMap>) {
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API as string,
+    })
+    
+    if (!isLoaded) return <CircularLoader />
+    return (
+        <GoogleMap
+            mapContainerStyle={{
+                width: '100%',
+                minHeight: '300px',
+            }}
+            center={{ lat, lng }}
+            zoom={10}
+        >
+        </GoogleMap>
+    )
+}
